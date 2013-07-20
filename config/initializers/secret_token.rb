2e5871a9a5ccfada673da7daaa9653d77cf217e2
Banikumar Maiti <banikumar.maiti@gmail.com>
2013-07-20 08:44:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TonightsMovie::Application.config.secret_token = '2eb40b66a454160c8b864f8df1fbcb3521f3a57604d0142e334c548c0ec08b15ea09a15191796d4de8d47f8ac60d4941fabb234b52b017d3c554553cc0530e6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TonightsMovie::Application.config.session_store :cookie_store, key: '_TonightsMovie_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TonightsMovie::Application.config.session_store :active_record_store
